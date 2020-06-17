4e5fa0415296f06bde3a91dce4a9babccc7c1953
Peter Simpson <simpson.petere@gmail.com>
2020-06-17 00:49:31 -0400

    updated privacy features in SnakeGame

--- lib/components/snek_game.dart
+++ lib/components/snek_game.dart
@@ -12,50 +12,56 @@ import '../constants.dart';
 
 class SnekGame extends Game with PanDetector {
   //Snake object which store's the snake's positions on the board
-  Snake snake = Snake(initialPositions: []);
+  Snake _snake = Snake(initialPositions: []);
 
   //Board object tracks individual tile states and is responsible for rendering them
-  Board board = Board();
+  Board _board = Board();
 
   //Flame widget which tracks the screen size
   Size screenSize;
 
   //Enum (see declaration in constants.dart) which tracks the snake's head travel direction
-  Direction snakeDirection = Direction.down;
+  Direction _snakeDirection = Direction.down;
 
   //double which is updated by the update() function, and controls how often we update the game state
-  double timeSinceLastUpdate = 0;
+  double _timeSinceLastUpdate = 0;
 
+  //Callback functions which can notify external widgets when a score or restart event is triggered
   Function onScore;
   Function onRestart;
 
   //value to track previous stepTime when game is paused
-  double oldStepTime = kStepTime;
+  double _oldStepTime = kStepTime;
 
   //double which tracks how long to wait between game state updates
-  double stepTime = kStepTime;
+  double _stepTime = kStepTime;
 
   //Random number generator to generate apple positions, seed with the time
-  Random rand = Random(DateTime.now().millisecondsSinceEpoch);
+  Random _rand = Random(DateTime.now().millisecondsSinceEpoch);
 
   //bool which represents whether or not the game is in acid mode
-  bool groovyMode = false;
+  bool _groovyMode = false;
 
   /*
   * List which is used to initialize the snake's positions
   * Should be contiguous, with the desired head position as the last element
   */
-  List<RowColPosition> initialSnakePositions = [
+  List<RowColPosition> _initialSnakePositions = [
     RowColPosition(row: 0, col: 3),
     RowColPosition(row: 1, col: 3),
     RowColPosition(row: 2, col: 3),
+
+    //uncomment for a longer initial snake--useful for testing purposes
+    /*
     RowColPosition(row: 3, col: 3),
     RowColPosition(row: 4, col: 3),
     RowColPosition(row: 5, col: 3),
     RowColPosition(row: 6, col: 3),
     RowColPosition(row: 7, col: 3),
+     */
   ];
-  RowColPosition applePosition = RowColPosition(row: 10, col: 5);
+
+  RowColPosition _applePosition = RowColPosition(row: 10, col: 5);
 
   bool firstRun = true;
 
@@ -71,42 +77,42 @@ class SnekGame extends Game with PanDetector {
 
   //function to pause the game by setting the step time to infinity
   void pause() {
-    stepTime = double.infinity;
+    _stepTime = double.infinity;
   }
 
   //Function which alters whether or not the game is in groovy mode
   void setGroovyMode(bool isGroovyModeEngaged) {
-    groovyMode = isGroovyModeEngaged;
-    board.setGroovyMode(isGroovyModeEngaged);
+    _groovyMode = isGroovyModeEngaged;
+    _board.setGroovyMode(isGroovyModeEngaged);
   }
 
   void setSpeed(bool isInTurboMode) {
     if (isInTurboMode) {
       print('Turbo Engaged');
-      stepTime = kStepTime / 3;
-      oldStepTime = stepTime;
+      _stepTime = kStepTime / 3;
+      _oldStepTime = _stepTime;
     } else {
-      oldStepTime = kStepTime;
-      stepTime = kStepTime;
+      _oldStepTime = kStepTime;
+      _stepTime = kStepTime;
     }
   }
 
   //resumes regular game updates by setting the steptime back to the original value
   void resume() {
-    stepTime = oldStepTime;
+    _stepTime = _oldStepTime;
   }
 
   @override
   void render(Canvas canvas) {
     if (screenSize == null) {
     } else {
-      board.render(canvas, screenSize);
+      _board.render(canvas, screenSize);
     }
   }
 
   @override
   void update(double t) {
-    timeSinceLastUpdate += t;
+    _timeSinceLastUpdate += t;
 
     //do nothing if we have not yet received a screenSize
     if (screenSize == null) {
@@ -119,10 +125,10 @@ class SnekGame extends Game with PanDetector {
       return;
     }
 
-    if (timeSinceLastUpdate >= stepTime) {
-      timeSinceLastUpdate = 0;
-      moveSnake();
-      board.update(t);
+    if (_timeSinceLastUpdate >= _stepTime) {
+      _timeSinceLastUpdate = 0;
+      _moveSnake();
+      _board.update(t);
     }
   }
 
@@ -152,14 +158,14 @@ class SnekGame extends Game with PanDetector {
     if (isHorizontal) {
       //a negative x velocity indicates a leftward drag
       if (xVelocity < 0) {
-        if (snakeDirection != Direction.right) {
-          snakeDirection = Direction.left;
+        if (_snakeDirection != Direction.right) {
+          _snakeDirection = Direction.left;
         }
       }
       //a positive x velocity indicates a rightward drag
       else {
-        if (snakeDirection != Direction.left) {
-          snakeDirection = Direction.right;
+        if (_snakeDirection != Direction.left) {
+          _snakeDirection = Direction.right;
         }
       }
     }
@@ -167,14 +173,14 @@ class SnekGame extends Game with PanDetector {
     else {
       //a negative y velocity indicates an upwards drag
       if (yVelocity < 0) {
-        if (snakeDirection != Direction.down) {
-          snakeDirection = Direction.up;
+        if (_snakeDirection != Direction.down) {
+          _snakeDirection = Direction.up;
         }
       }
       //a positive y velocity indicates a downwards drag
       else {
-        if (snakeDirection != Direction.up) {
-          snakeDirection = Direction.down;
+        if (_snakeDirection != Direction.up) {
+          _snakeDirection = Direction.down;
         }
       }
     }
@@ -190,52 +196,52 @@ class SnekGame extends Game with PanDetector {
 
   void initialize() {
     //initialize the board
-    board.initialize(screenSize);
+    _board.initialize(screenSize);
 
     //initialize the snake
-    for (RowColPosition position in initialSnakePositions) {
-      snake.addHead(position);
-      board.flipSnakePresence(position);
+    for (RowColPosition position in _initialSnakePositions) {
+      _snake.addHead(position);
+      _board.flipSnakePresence(position);
     }
 
     //initialize the apple
-    board.flipApplePresencse(applePosition);
+    _board.flipApplePresencse(_applePosition);
   }
 
   //adjusts the snake to reflect a one-tile shift in snakeDirection, then
   //updates board tiles accordingly
-  void moveSnake() {
+  void _moveSnake() {
     RowColPosition nextHeadPos = RowColPosition();
-    RowColPosition oldHeadPos = snake.getHead();
+    RowColPosition oldHeadPos = _snake.getHead();
 
     //update head position
     //handle downward motion case
-    if (snakeDirection == Direction.down) {
+    if (_snakeDirection == Direction.down) {
       nextHeadPos.row = oldHeadPos.row + 1;
       nextHeadPos.col = oldHeadPos.col;
 
       //account for bottom-to-top wraparound
-      if (nextHeadPos.row >= board.numberOfVerticalTiles) {
+      if (nextHeadPos.row >= _board.numberOfVerticalTiles) {
         nextHeadPos.row = 0;
       }
     }
     //handle upward motion case
-    else if (snakeDirection == Direction.up) {
+    else if (_snakeDirection == Direction.up) {
       nextHeadPos.row = oldHeadPos.row - 1;
       nextHeadPos.col = oldHeadPos.col;
 
       //account for bottom-to-top wraparound
       if (nextHeadPos.row < 0) {
-        nextHeadPos.row = board.numberOfVerticalTiles - 1;
+        nextHeadPos.row = _board.numberOfVerticalTiles - 1;
       }
     }
     //handle rightward motion case
-    else if (snakeDirection == Direction.right) {
+    else if (_snakeDirection == Direction.right) {
       nextHeadPos.row = oldHeadPos.row;
       nextHeadPos.col = oldHeadPos.col + 1;
 
       //account for right-to-left wraparound
-      if (nextHeadPos.col >= board.numberOfHorizontalTiles) {
+      if (nextHeadPos.col >= _board.numberOfHorizontalTiles) {
         nextHeadPos.col = 0;
       }
     }
@@ -246,32 +252,32 @@ class SnekGame extends Game with PanDetector {
 
       //account for right-to-left wraparound
       if (nextHeadPos.col < 0) {
-        nextHeadPos.col = board.numberOfHorizontalTiles - 1;
+        nextHeadPos.col = _board.numberOfHorizontalTiles - 1;
       }
     }
 
     //check to see if the snake has hit itself (game end condition)
-    if (snake.isPositionInSnake(nextHeadPos)) {
+    if (_snake.isPositionInSnake(nextHeadPos)) {
       onRestart();
     }
 
     //inform snake of updated head position
-    snake.addHead(nextHeadPos);
+    _snake.addHead(nextHeadPos);
 
     //check to see if the snake his hit the apple
-    if (applePosition == snake.getHead()) {
+    if (_applePosition == _snake.getHead()) {
       //if so, deallocate that square as the apple, and make it a part of the snake
-      board.flipApplePresencse(snake.getHead());
-      board.flipSnakePresence(snake.getHead());
+      _board.flipApplePresencse(_snake.getHead());
+      _board.flipSnakePresence(_snake.getHead());
 
       //call callback passed in as a parameter to adjust score
       onScore();
 
       //generate a new apple position
-      newApple();
+      _newApple();
 
       //tell the board to render the new apple
-      board.flipApplePresencse(applePosition);
+      _board.flipApplePresencse(_applePosition);
 
       //we can finish here since we're not removing the tail
       return;
@@ -279,45 +285,46 @@ class SnekGame extends Game with PanDetector {
 
     //inform board that the old tail should no longer be rendered
 
-    board.flipSnakePresence(snake.getTail());
+    _board.flipSnakePresence(_snake.getTail());
     //tell board to render new snake head
-    board.flipSnakePresence(snake.getHead());
+    _board.flipSnakePresence(_snake.getHead());
 
     //update snake tail position
-    snake.popTail();
+    _snake.popTail();
   }
 
   //assumes there is no longer a valid apple on the board, and creates a new one
-  void newApple() {
+  void _newApple() {
     //do not generate a new apple if the snake now takes up the entire board
     //(will result in an infinite loop otherwise)
-    if (snake.size >=
-        (board.numberOfHorizontalTiles * board.numberOfHorizontalTiles)) {
-      applePosition = null;
+    if (_snake.size >=
+        (_board.numberOfHorizontalTiles * _board.numberOfHorizontalTiles)) {
+      _applePosition = null;
       return;
     }
 
-    int rowCoordinate = rand.nextInt(board.numberOfVerticalTiles);
-    int colCoordinate = rand.nextInt(board.numberOfHorizontalTiles);
+    int rowCoordinate = _rand.nextInt(_board.numberOfVerticalTiles);
+    int colCoordinate = _rand.nextInt(_board.numberOfHorizontalTiles);
 
     //re-generate the apple if it's outside of the safe area
     if (rowCoordinate <= 3) {
-      newApple();
+      _newApple();
     }
     //if the newly-generated apple conflicts with the snake, get a new position
-    else if (snake.isPositionInSnake(
+    else if (_snake.isPositionInSnake(
         RowColPosition(row: rowCoordinate, col: colCoordinate))) {
-      newApple();
+      _newApple();
     }
     //otherwise set the apple to that position
     else {
-      applePosition = RowColPosition(row: rowCoordinate, col: colCoordinate);
+      _applePosition = RowColPosition(row: rowCoordinate, col: colCoordinate);
     }
   }
 
+  //Function called by Flame to set canvas size
   @override
   void resize(Size size) {
     screenSize = size;
-    board.setScreenSize(size);
+    _board.setScreenSize(size);
   }
 }
